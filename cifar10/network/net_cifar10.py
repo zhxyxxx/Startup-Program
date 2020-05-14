301989a6f78fabf10578ce2ce1b0540480926f34
zhxyxxx <41413243+zhxyxxx@users.noreply.github.com>
2020-05-14 23:51:07 +0900

    update main program

--- cifar10/network/net_cifar10.py
+++ cifar10/network/net_cifar10.py
@@ -5,21 +5,30 @@ from torchvision import models
 from torch import nn
 from torch.functional import F
 from torch import optim
+from efficientnet_pytorch import EfficientNet
 import matplotlib.pyplot as plt
 import argparse
 import time
-from efficientnet_pytorch import EfficientNet
+import logging
 
-NET = ["Lenet", "Mobilenet", "VGG", "Resnet", "Efficientnet"]
+logging.basicConfig(filename='net_logger.log', level=logging.INFO)
+NET = ["Lenet", "Lenet_r", "Mobilenet", "VGG", "VGG_bn", "Resnet50", "Resnet101", "Efficientnet"]
 
+# command line argument
 parser = argparse.ArgumentParser()
 parser.add_argument('network', choices=NET)
 parser.add_argument('-l', '--lr', type=float, default=0.005)
 parser.add_argument('-e', '--epoch', type=int, default=50)
+parser.add_argument('--m', type=float, default=0)
+parser.add_argument('--wd', type=float, default=0)
+parser.add_argument('--test', action='store_true')
+parser.add_argument('--load', action='store_true')
 args = parser.parse_args()
 
+model_file = args.network + "_cifar10.ckpt"
+fig_file = args.network + "_cifar10.png"
 network = NET.index(args.network)
-if network == 0:
+if network <= 1:
     transform = transforms.ToTensor()
 else:
     transform = transforms.Compose([
@@ -27,6 +36,7 @@ else:
         transforms.ToTensor()
     ])
 
+# prepare data
 train_Data = dsets.CIFAR10(
     root='../data_cifar10',
     train=True,
@@ -43,7 +53,6 @@ test_data = dsets.CIFAR10(
 
 train_data, valid_data = torch.utils.data.random_split(train_Data, [40000, 10000])
 
-#load data
 train_loader = torch.utils.data.DataLoader(
     dataset=train_data,
     batch_size=64,
@@ -87,23 +96,58 @@ class Lenet(nn.Module):
         x = F.relu(self.fc1(x))
         return self.fc2(x)
 
+#Lenet with regularization
+class Lenet_r(nn.Module):
+    def __init__(self):
+        super(Lenet_r, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 120, 5)
+        self.pool = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
+        self.drop = nn.Dropout()
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 1*1*120)
+        x = F.relu(self.fc1(x))
+        x = self.drop(x)
+        return self.fc2(x)
+
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# choose network
 if network == 0:
     net = Lenet().to(device)
 elif network == 1:
-    net = models.mobilenet_v2(num_classes=10).to(device)
+    net = Lenet_r().to(device)
 elif network == 2:
-    net = models.vgg16(num_classes=10).to(device)
+    net = models.mobilenet_v2(num_classes=10).to(device)
 elif network == 3:
-    net = models.resnet50(num_classes=10).to(device)
+    net = models.vgg16(num_classes=10).to(device)
 elif network == 4:
+    net = models.vgg16_bn(num_classes=10).to(device)
+elif network == 5:
+    net = models.resnet50(num_classes=10).to(device)
+elif network == 6:
+    net = models.resnet101(num_classes=10).to(device)
+elif network == 7:
     net = EfficientNet.from_name('efficientnet-b0', override_params={'num_classes': 10}).to(device)
 else:
     print("Error")
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr)
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.m, weight_decay=args.wd)
+
+if args.load:
+    net.load_state_dict(torch.load(model_file))
 
 print(args.network)
 
@@ -158,6 +202,7 @@ for epoch in range(num_epochs):
 timeperepoch = elapsed_time / num_epochs
 print('elapsed time per epoch: {:.2f}'.format(timeperepoch))
 
+
 test_acc = 0
 
 #test
@@ -168,30 +213,39 @@ with torch.no_grad():
         outputs = net(inputs)
         acc = (outputs.max(1)[1] == labels).sum()
         test_acc += acc.item()
-    print('test_accuracy: {} %'.format(100 * test_acc / len(test_loader.dataset)))
+    accuracy = 100 * test_acc / len(test_loader.dataset)
+    print('test_accuracy: {} %'.format(accuracy))
 
+if not args.test:
+    # write log
+    logging.info('Using {} with lr: {}, epochs: {}, m: {}, wd: {}'
+            .format(args.network, args.lr, args.epoch, args.m, args.wd))
+    logging.info('tl: {}'.format(train_loss_list))
+    logging.info('ta: {}'.format(train_acc_list))
+    logging.info('vl: {}'.format(val_loss_list))
+    logging.info('va: {}'.format(val_acc_list))
+    logging.info('elapsed time: {}, accuracy: {}'.format(timeperepoch, accuracy))
 
-model_file = args.network + "_cifar10.ckpt"
-torch.save(net.state_dict(), model_file)  #save model
+    # save model
+    torch.save(net.state_dict(), model_file)
 
-#plot
-fig_file = args.network + "_cifar10.png"
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10,4))
+    # plot
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10,4))
 
-ax1.plot(range(num_epochs), train_loss_list, color='blue', linestyle='-', label='train_loss')
-ax1.plot(range(num_epochs), val_loss_list, color='green', linestyle='--', label='val_loss')
-ax1.legend()
-ax1.set_xlabel('epoch')
-ax1.set_ylabel('loss')
-ax1.set_title('Training and validation loss')
-ax1.grid()
+    ax1.plot(range(num_epochs), train_loss_list, color='blue', linestyle='-', label='train_loss')
+    ax1.plot(range(num_epochs), val_loss_list, color='green', linestyle='--', label='val_loss')
+    ax1.legend()
+    ax1.set_xlabel('epoch')
+    ax1.set_ylabel('loss')
+    ax1.set_title('Training and validation loss')
+    ax1.grid()
 
-ax2.plot(range(num_epochs), train_acc_list, color='blue', linestyle='-', label='train_acc')
-ax2.plot(range(num_epochs), val_acc_list, color='green', linestyle='--', label='val_acc')
-ax2.legend()
-ax2.set_xlabel('epoch')
-ax2.set_ylabel('acc')
-ax2.set_title('Training and validation accuracy')
-ax2.grid()
+    ax2.plot(range(num_epochs), train_acc_list, color='blue', linestyle='-', label='train_acc')
+    ax2.plot(range(num_epochs), val_acc_list, color='green', linestyle='--', label='val_acc')
+    ax2.legend()
+    ax2.set_xlabel('epoch')
+    ax2.set_ylabel('acc')
+    ax2.set_title('Training and validation accuracy')
+    ax2.grid()
 
-fig.savefig(fig_file)
+    fig.savefig(fig_file)
